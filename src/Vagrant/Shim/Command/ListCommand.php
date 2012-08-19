c0537d8705a77bbbfed18488c85954fbe5cf7db8
Eric Clemmons <eric@smarterspam.com>
2012-08-19 15:21:04 -0500

    Removed unused use statements

--- src/Vagrant/Shim/Command/ListCommand.php
+++ src/Vagrant/Shim/Command/ListCommand.php
@@ -2,10 +2,7 @@
 
 namespace Vagrant\Shim\Command;
 
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ListCommand extends Command
