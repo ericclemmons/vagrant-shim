e5aadace7b305b27353c6f6a47ca95b2433daac5
Eric Clemmons <eric@smarterspam.com>
2012-08-19 14:30:29 -0500

    Shim supports space-delimited input and asks for confirmation

--- src/Vagrant/Shim/Command/ShimCommand.php
+++ src/Vagrant/Shim/Command/ShimCommand.php
@@ -20,22 +20,30 @@ class ShimCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $programs = $input->getArgument('programs');
+        $programs   = $input->getArgument('programs');
+        $dialog     = $this->getHelperSet()->get('dialog');
 
         // Gather list of programs
         if (empty($programs)) {
-            $dialog     = $this->getHelperSet()->get('dialog');
             $question   = 'Specify a program (or <comment>ENTER</comment> when done): ';
 
             while ($program = $dialog->ask($output, $question)) {
-                $programs[] = trim($program);
+                $programs = array_merge($programs, explode(' ', trim($program)));
             }
         }
 
-        // Shiim programs
-        foreach ($programs as $program) {
-            if ($this->manager->createShim($program)) {
-                $output->writeln(sprintf('Created shim for <info>%s</info>', $program));
+        $question = sprintf(
+            'Shim <comment>%s</comment> programs: <info>%s</info>? [<comment>Y</comment>/n] ',
+            count($programs),
+            join('</info>, <info>', $programs)
+        );
+
+        if ($dialog->askConfirmation($output, $question)) {
+            // Shim programs
+            foreach ($programs as $program) {
+                if ($this->manager->createShim($program)) {
+                    $output->writeln(sprintf('Created shim for <info>%s</info>', $program));
+                }
             }
         }
     }
