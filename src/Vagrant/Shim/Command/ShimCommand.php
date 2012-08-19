aaa31ef586c14a5204f0b9eac7b7a33bdd9f6110
Eric Clemmons <eric@smarterspam.com>
2012-08-19 15:04:31 -0500

    Simplified shim command and prevented shimming of php

--- src/Vagrant/Shim/Command/ShimCommand.php
+++ src/Vagrant/Shim/Command/ShimCommand.php
@@ -14,36 +14,21 @@ class ShimCommand extends Command
         $this
             ->setName('shim')
             ->setDescription('Specify programs to shim through Vagrant')
-            ->addArgument('programs', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Programs shim through to Vagrant')
+            ->addArgument('programs', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Programs shim through to Vagrant')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $programs   = $input->getArgument('programs');
-        $dialog     = $this->getHelperSet()->get('dialog');
+        $programs = $input->getArgument('programs');
 
-        // Gather list of programs
-        if (empty($programs)) {
-            $question   = 'Specify a program (or <comment>ENTER</comment> when done): ';
-
-            while ($program = $dialog->ask($output, $question)) {
-                $programs = array_merge($programs, explode(' ', trim($program)));
-            }
+        if (in_array('php', $programs)) {
+            throw new \InvalidArgumentException('PHP cannot be shimmed for vagrant-shim to work!');
         }
 
-        $question = sprintf(
-            'Shim <comment>%s</comment> programs: <info>%s</info>? [<comment>Y</comment>/n] ',
-            count($programs),
-            join('</info>, <info>', $programs)
-        );
-
-        if ($dialog->askConfirmation($output, $question)) {
-            // Shim programs
-            foreach ($programs as $program) {
-                if ($this->manager->createShim($program)) {
-                    $output->writeln(sprintf('Created shim for <info>%s</info>', $program));
-                }
+        foreach ($programs as $program) {
+            if ($this->manager->createShim($program)) {
+                $output->writeln(sprintf('Created shim for <info>%s</info>', $program));
             }
         }
     }
